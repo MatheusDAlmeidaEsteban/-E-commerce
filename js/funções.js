c12e3800714be8ff24dc42b5cf9cf954ba3ca9f3
matheus <matheusestebam123@gmail.com>
2022-04-29 14:06:08 -0300

    js

--- js/funções.js
+++ js/funções.js
@@ -10,3 +10,34 @@ buscar.addEventListener("click",(e)=>{
         pesquisar.style.transition =  ".5s";
     }
 })
+
+//slide 
+let time = 3000,
+    currentImagemIndex =0 ;
+    imagens = document.querySelectorAll("#slide img");
+    max = imagens.length;
+    navBar = document.getElementById("navBar")
+    var coordenadasNavBar = navBar.getBoundingClientRect();
+   
+function nextImg(){
+    imagens[currentImagemIndex]
+        .classList.remove("select");
+
+    currentImagemIndex ++
+
+    if(currentImagemIndex >= max){
+        currentImagemIndex = 0;
+    }
+
+    imagens[currentImagemIndex].classList.add("select");
+
+   
+
+
+}
+function start(){
+    setInterval(() =>{
+        nextImg();
+    },time)
+}
+window.addEventListener("load", start,);
